fc51ad1ff9751ce7c5287fc8d9e50c2a6189b0ad
MatisVivier <matisvivier2004@gmail.com>
2025-08-27 20:57:09 +0200

    socket2

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,12 +1,9 @@
-import { io } from 'socket.io-client';
+import io from 'socket.io-client';
 
 export const socket = io('https://fast-type-back.onrender.com', {
   withCredentials: true,
-  // laissez le polling possible pour établir la connexion puis upgrade en WS
   transports: ['polling', 'websocket'],
-  // explicite mais optionnel (c’est la valeur par défaut)
   path: '/socket.io',
-  // plus tolérant aux réseaux “capricieux”
   reconnection: true,
   reconnectionAttempts: 10,
   reconnectionDelay: 500,
