018300227d4dc1b70fbe4ca1694d319c425e36b6
MatisVivier <matisvivier2004@gmail.com>
2025-08-28 13:02:08 +0200

    front

--- src/components/AccountPanel.jsx
+++ src/components/AccountPanel.jsx
@@ -83,7 +83,7 @@ export default function AccountPanel({ user, onUserUpdate }) {
           display:grid;
           grid-template-columns: repeat(5, minmax(0,1fr));
           gap:12px;
-          margin-top:12px;
+          margin-top:-20%;
         }
         @media (max-width: 1200px){ .matchGrid { grid-template-columns: repeat(3, minmax(0,1fr)); } }
         @media (max-width: 800px){ .matchGrid { grid-template-columns: repeat(2, minmax(0,1fr)); } }
